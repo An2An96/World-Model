c7458891d10ed619e0b8494131e6565578923f71
An2An <an2an96@bk.ru>
2015-04-17 23:54:34 +0300

    Update predator.cpp

--- predator.cpp
+++ predator.cpp
@@ -1,41 +1,111 @@
 #include "world.h"
 
-World::Predator::Predator(int birtch_ac, int birtch_c, int eat_c, int death_s) :
-	Entity(birtch_ac, birtch_c) 
+World::Predator::Predator(World* world, int x, int y, int birtch_ac, int birtch_c, int eat_c, int death_s) :
+	Entity(world, x, y, birtch_ac, birtch_c)
 {
 	ChanceEatVictim = eat_c;
 	StepToDeath = death_s;
+
+	gWorld->predatorsCount++;
 }
 
-World::Predator::Predator() :
-	Entity()
+World::Predator::Predator(World* world, int x, int y) :
+	Entity(world, x, y)
 {
 	ChanceEatVictim = random(50, 100);
 	StepToDeath = random(10, 30);
+
+	gWorld->predatorsCount++;
 }
 
 World::Predator::~Predator()
 {
+	gWorld->predatorsCount--;
 }
 
-E_STEP World::Predator::Step(bool eat = false)
+int World::Predator::Step(bool eat = false)
 {
-	if (eat)	{
-		StepWithoutFood = 0;
-		if (++ActionForBirth >= BirthActionCount)	{
-			if (random(0, 100) <= ChanceBirth)	{
-				//	рождаем новую жертву
-				ActionForBirth = 0;
-				return STEP_BIRTH;
+	int bufX, bufY, rand;
+	bool chPos = false;
+	std::vector<int> pos;
+	if (gWorld->Calculate(this->pos[0], this->pos[1], pos))	{
+		for (int s = pos.size() - 1; s >= 0; s--)	{
+			rand = random(0, pos.size() - 1);
+			gWorld->Size(pos[rand], bufX, bufY);
+
+			if (gWorld->InCell(bufX, bufY) == ENTITY_VICTIMS || gWorld->InInterim(bufX, bufY) == ENTITY_VICTIMS)	{
+				
+				if (random(0, 100) <= ChanceEatVictim)	{
+
+					chPos = true;
+
+					this->pos[0] = bufX; this->pos[1] = bufY;
+
+					StepWithoutFood = 0;
+					ActionForBirth++;
+
+					Victim* v;
+					if ((v = dynamic_cast<Victim*>(gWorld->gEntitys[gWorld->Cell(bufX, bufY)].get())) == NULL)	{
+						v = dynamic_cast<Victim*>(gWorld->gInterim[gWorld->Cell(bufX, bufY)].get());
+					}
+
+					gWorld->gInterim.erase(gWorld->Cell(bufX, bufY));	gWorld->gEntitys.erase(gWorld->Cell(bufX, bufY));
+
+					//	Проверка на отравление
+					if (random(0, 100) <= v->GetPoisoning())	{
+
+						return -1;
+					}
+					break;
+				}
 			}
 		}
-		return STEP_EAT;
+		if (chPos == false)	{
+			do
+			{
+				rand = random(0, pos.size() - 1);
+				gWorld->Size(pos[rand], bufX, bufY);
+
+				//	Если в клетке никого нет
+				if (gWorld->InCell(bufX, bufY) == ENTITY_NONE && gWorld->InInterim(bufX, bufY) == ENTITY_NONE)	{
+
+					this->pos[0] = bufX; this->pos[1] = bufY;
+
+					if (++StepWithoutFood >= StepToDeath)	{
+						//	убиваем хищника
+						return -1;
+					}
+					break;
+				}
+				else	{
+					pos.erase(pos.begin() + rand);
+				}
+			} while (pos.size());
+		}
+		if (ActionForBirth >= BirthActionCount && random(0, 100) <= ChanceBirth)	{
+			//	рождаем новую жертву
+			ActionForBirth = 0;
+
+			//	вычесляем позицию и рождаем на ней новое существо
+			pos.clear();
+			if (gWorld->Calculate(this->pos[0], this->pos[1], pos))	{
+				do
+				{
+					rand = random(0, pos.size() - 1);
+					gWorld->Size(pos[rand], bufX, bufY);
+
+					if (gWorld->InCell(bufX, bufY) == ENTITY_NONE && gWorld->InInterim(bufX, bufY) == ENTITY_NONE)	{
+						gWorld->CreateEntity(ENTITY_PREDATORS, bufX, bufY);
+						break;
+					}
+					else {
+						pos.erase(pos.begin() + rand);
+					}
+				} while (pos.size());
+			}
+		}
 	}
-	else if (++StepWithoutFood >= StepToDeath)	{
-		//	убиваем хищника
-		return STEP_DEATH;
-	}
-	return STEP_NONE;
+	return gWorld->Cell(this->pos[0], this->pos[1]);
 }
 
 void World::Predator::View()
