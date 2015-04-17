e880fb58505bf9f24b72d6dbcdce1a0c429ca0c4
An2An <an2an96@bk.ru>
2015-04-17 23:52:45 +0300

    Update entity.cpp

--- entity.cpp
+++ entity.cpp
@@ -1,13 +1,17 @@
 #include "world.h"
 
-World::Entity::Entity()
+World::Entity::Entity(World* world, int x, int y)
 {
+	gWorld = world;
+	pos[0] = x;	pos[1] = y;
 	BirthActionCount = random(5, 20);
 	ChanceBirth = random(50, 100);
 }
 
-World::Entity::Entity(int birtch_ac, int birtch_c)
+World::Entity::Entity(World* world, int x, int y, int birtch_ac, int birtch_c)
 {
+	gWorld = world;
+	pos[0] = x;	pos[1] = y;
 	BirthActionCount = birtch_ac;
 	ChanceBirth = birtch_c;
 }
