f7ebccd267de92c25d78e4304259218e494b243f
An2An <an2an96@bk.ru>
2015-04-17 23:53:20 +0300

    Update world.cpp

--- world.cpp
+++ world.cpp
@@ -23,13 +23,11 @@ bool World::CreateEntity(E_ENTITY type, int x, int y)
 	if (x >= 0 && x < sizeX && y >= 0 && y < sizeY && InCell(x, y) == ENTITY_NONE)
 	{
 		if (type == ENTITY_PREDATORS)	{
-			gEntitys[Cell(x, y)] = new Predator();
-			predatorsCount++;
+			gEntitys[Cell(x, y)] = std::make_unique<World::Predator>(this, x, y);
 			return true;
 		}
 		else if (type == ENTITY_VICTIMS)	{
-			gEntitys[Cell(x, y)] = new Victim();
-			victimsCount++;
+			gEntitys[Cell(x, y)] = std::make_unique<World::Victim>(this, x, y);
 			return true;
 		}
 	}
@@ -38,157 +36,40 @@ bool World::CreateEntity(E_ENTITY type, int x, int y)
 
 bool World::PerformStep()
 {
-	int bufX, bufY, nX, nY, rand;
-	E_STEP step = STEP_NONE;
-
 	//	@Step Victims
-
 	if (gCycle == ENTITY_VICTIMS)	{
-		//	Ищем из списка существ, только жертв
-		for (int y = 0; y < sizeY; y++)	{
-			for (int x = 0; x < sizeX; x++)	{
-				nX = x; nY = y;
-				if (dynamic_cast<Victim*>(gEntitys[Cell(x,y)]))	{
-					//	подбираем позицию для хода
-					std::vector<int> pos;
-					if (Calculate(x, y, pos))	{
-						do
-						{
-							rand = random(0, pos.size() - 1);
-							bufY = pos[rand] / sizeX; bufX = pos[rand] % sizeX;
 
-							//	Если в клетке никого нет
-							if (InCell(bufX, bufY) == ENTITY_NONE && InInterim(bufX, bufY) == ENTITY_NONE)	{
-								step = (*gEntitys[Cell(x, y)]).Step();
-								nX = bufX; nY = bufY;
-
-								if (step == STEP_BIRTH)	{
-									//	вычесляем позицию и рождаем на ней новое существо
-									pos.clear();
-									if (Calculate(nX, nY, pos))	{
-										do
-										{
-											rand = random(0, pos.size() - 1);
-											bufY = pos[rand] / sizeX; bufX = pos[rand] % sizeX;
-
-											if (InCell(bufX, bufY) == ENTITY_NONE && InInterim(bufX, bufY) == ENTITY_NONE)	{
-												CreateEntity(ENTITY_VICTIMS, nX, nY);
-												break;
-											} else {
-												pos.erase(pos.begin() + rand);
-											}
-										} while (pos.size());
-									}
-								}
-								break;
-							} else	{
-								pos.erase(pos.begin() + rand);
-							}
-						} while (pos.size());
-					}
-					gInterim[Cell(nX, nY)] = gEntitys[Cell(x, y)];
-					gEntitys.erase(Cell(x, y));
-				}
-				else if (dynamic_cast<Predator*>(gEntitys[Cell(x, y)]))	{
-					gInterim[Cell(nX, nY)] = gEntitys[Cell(x, y)];
-					gEntitys.erase(Cell(x, y));
-				}
+		for (auto i = gEntitys.begin(); i != gEntitys.end(); i = gEntitys.begin())
+		{
+			if (dynamic_cast<Victim*>(i->second.get()))	{
+				int cell = i->second->Step();
+				gInterim[cell] = std::move(gEntitys[i->first]);
 			}
+			else if (dynamic_cast<Predator*>(i->second.get()))	{
+				gInterim[i->first] = std::move(gEntitys[i->first]);
+			}
+			gEntitys.erase(i);
 		}
 		gCycle = ENTITY_PREDATORS;
 	}
 
 //	@Step Predators
-
 	else if (gCycle == ENTITY_PREDATORS)	{
 
-		//	Ищем из списка существ, только хищников
-		for (int y = 0; y < sizeY; y++)	{
-			for (int x = 0; x < sizeX; x++)	{
-				nX = x; nY = y;
-				if (dynamic_cast<Predator*>(gEntitys[Cell(x, y)]))	{
-					//	подбираем позицию для хода
-					std::vector<int> pos;
-					if (Calculate(x, y, pos))	{
-						for (int s = pos.size() - 1; s >= 0; s--)	{
-							bufY = pos[s] / sizeX; bufX = pos[s] % sizeX;
-
-							if (InCell(bufX, bufY) == ENTITY_VICTIMS || InInterim(bufX, bufY) == ENTITY_VICTIMS)	{
-								step = (*gEntitys[Cell(x, y)]).Step(true);
-
-								if (step == STEP_BIRTH || step == STEP_EAT)	{
-
-									nX = bufX; nY = bufY;
-
-									Victim* v = dynamic_cast<Victim*>(gEntitys[Cell(nX, nY)]);
-									if ((v = dynamic_cast<Victim*>(gEntitys[Cell(nX, nY)])) == NULL)	{
-										v = dynamic_cast<Victim*>(gInterim[Cell(nX, nY)]);
-									}
-									//	Проверка на отравление
-									if (random(0, 100) <= (*v).GetPoisoning())	{
-										step = STEP_DEATH;
-									}
-
-									delete gInterim[Cell(nX, nY)],	gEntitys[Cell(nX, nY)];
-									gInterim.erase(Cell(nX, nY));	gEntitys.erase(Cell(nX, nY));
-									victimsCount--;
-									break;
-								}
-							}
-						}
-						if (nX == x && nY == y)	{
-							do
-							{
-								rand = random(0, pos.size() - 1);
-								bufY = pos[rand] / sizeX; bufX = pos[rand] % sizeX;
-
-								//	Если в клетке никого нет
-								if (InCell(bufX, bufY) == ENTITY_NONE && InInterim(bufX, bufY) == ENTITY_NONE)	{
-									step = (*gEntitys[Cell(x, y)]).Step();
-									nX = bufX; nY = bufY;
-									break;
-								}
-								else	{
-									pos.erase(pos.begin() + rand);
-								}
-							} while (pos.size());
-						}
-						if (step == STEP_BIRTH)	{
-							//	вычесляем позицию и рождаем на ней новое существо
-							pos.clear();
-							if (Calculate(nX, nY, pos))	{
-								do
-								{
-									rand = random(0, pos.size() - 1);
-									bufY = pos[rand] / sizeX; bufX = pos[rand] % sizeX;
-
-									if (InCell(bufX, bufY) == ENTITY_NONE && InInterim(bufX, bufY) == ENTITY_NONE)	{
-										CreateEntity(ENTITY_PREDATORS, nX, nY);
-										break;
-									}
-									else {
-										pos.erase(pos.begin() + rand);
-									}
-								} while (pos.size());
-							}
-						}
-						else if (step == STEP_DEATH)	{
-							delete gEntitys[Cell(x, y)];
-							gEntitys.erase(Cell(x, y));
-							predatorsCount--;
-							continue;
-						}
-					}
-					gInterim[Cell(nX, nY)] = gEntitys[Cell(x, y)];
-					gEntitys.erase(Cell(x, y));
-				}
-				else if (dynamic_cast<Victim*>(gEntitys[Cell(x, y)]))	{
-					gInterim[Cell(nX, nY)] = gEntitys[Cell(x, y)];
-					gEntitys.erase(Cell(x, y));
+		for (auto i = gEntitys.begin(); i != gEntitys.end(); i = gEntitys.begin())
+		{
+			if (dynamic_cast<Predator*>(i->second.get()))	{
+				int cell = i->second->Step();
+				if (cell != (-1))	{
+					gInterim[cell] = std::move(gEntitys[i->first]);
 				}
 			}
+			else if (dynamic_cast<Victim*>(i->second.get()))	{
+				gInterim[i->first] = std::move(gEntitys[i->first]);
+			}
+			gEntitys.erase(i);
 		}
-		gCycle = ENTITY_VICTIMS;
+		gCycle = ENTITY_VICTIMS; 
 	}
 	gInterim.swap(gEntitys);
 	gInterim.clear();
@@ -208,13 +89,23 @@ bool World::Calculate(int curx, int cury, std::vector<int>& pos)
 	return pos.size() ? true : false;
 }
 
+void World::Size(int size, int& sx, int& sy)
+{
+	sy = size / sizeX; sx = size % sizeX;
+}
+
+int World::Cell(int x, int y)
+{
+	return (y * sizeX + x);
+}
+
 E_ENTITY World::InCell(int x, int y)
 {
 	if (gEntitys[Cell(x, y)]) {
-		if (dynamic_cast<Victim*>(gEntitys[Cell(x, y)]))	{
+		if (dynamic_cast<Victim*>(gEntitys[Cell(x, y)].get()))	{
 			return ENTITY_VICTIMS;
 		}
-		else if (dynamic_cast<Predator*>(gEntitys[Cell(x, y)]))	{
+		else if (dynamic_cast<Predator*>(gEntitys[Cell(x, y)].get()))	{
 			return ENTITY_PREDATORS;
 		}
 	}
@@ -224,10 +115,10 @@ E_ENTITY World::InCell(int x, int y)
 E_ENTITY World::InInterim(int x, int y)
 {
 	if (gInterim[Cell(x, y)])	{
-		if (dynamic_cast<Victim*>(gInterim[Cell(x, y)]))	{
+		if (dynamic_cast<Victim*>(gInterim[Cell(x, y)].get()))	{
 			return ENTITY_VICTIMS;
 		}
-		else if (dynamic_cast<Predator*>(gInterim[Cell(x, y)]))	{
+		else if (dynamic_cast<Predator*>(gInterim[Cell(x, y)].get()))	{
 			return ENTITY_PREDATORS;
 		}
 	}
@@ -236,21 +127,20 @@ E_ENTITY World::InInterim(int x, int y)
 
 void World::View()
 {
-	for (int x = 0; x < sizeX + 2; x++)	printf("_");
-	printf("\n");
+	for (int x = 0; x < sizeX + 2; x++)	std::cout << "_";
+	std::cout << std::endl;
 	for (int y = 0; y < sizeY; y++)	{
-		printf("|");
+		std::cout << "|";
 		for (int x = 0; x < sizeX; x++)	{
 			if (gEntitys[Cell(x, y)])	{
-				(*gEntitys[Cell(x, y)]).View();
+				gEntitys[Cell(x, y)]->View();
 			} else {
-				printf(" ");
+				std::cout << " ";
 			}
 		}
-		printf("|\n");
+		std::cout << "|" << std::endl;
 	}
-	for (int x = 0; x < sizeX + 2; x++)	printf("_");
-	printf("\n");
+	for (int x = 0; x < sizeX + 2; x++)	std::cout << "_";
 
-	printf("p = %d, v = %d", predatorsCount, victimsCount);
+	std::cout << std::endl << "Predators: " << predatorsCount << ", Victims: " << victimsCount << std::endl;
 }
