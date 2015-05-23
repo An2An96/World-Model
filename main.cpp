81c0b7f1af0dcb8750aa1633ce9a5f91b3aac745
An2An <an2an96@bk.ru>
2015-05-23 22:19:14 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,87 +3,15 @@
 #include <stdio.h>
 #include <conio.h>
 #include <time.h>
-
 #include "useful.h"
 #include "world.h"
+#include "view.h"
 
-using namespace std;
-
-int main()
+int main(int argc, char* argv[])
 {
-	//	settings
-	setlocale(LC_ALL, "russian");
-	srand((int)time(NULL));
-	
-	World* w = 0;
-	cout << "1 - создать новое поле; 2 - загрузить из файла" << endl;
-	char c = _getch();
-	char* fname = new char[200];
-	if (c == '1')
-	{
-		int x, y;
-		cout << "Введите размеры поля" << endl;
-		cin >> x >> y;
-		w = new World(x, y);
-	}
-	else if(c == '2')
-	{
-		cout << "Введите имя файла для чтения: ";
-		std::cin.getline(fname, 200);
-		w = new World(fname);
-	}
-	if (!w)	{
-		return false;
-	}
-	std::cout << "1 - сохранить; 2 - пауза; Esc - выход\n";
-	system("pause");
-	do
-	{
-		if (_kbhit())
-		{
-			c = _getch();
-			if (c == '1')
-			{
-				cout << "Введите имя файла для записи: ";
-				std::cin.get();
-				std::cin.getline(fname, 200);
-				bool is_saved = w->SaveWorld(fname);
-				std::cout << "Сохранено\n";
-				system("pause");
-			}
-			else if(c == '2')
-			{
-				c = _getch();
-				if (c == '1')
-				{
-					cout << "Введите имя файла для записи: ";
-					cin.get();
-					cin.getline(fname, 200);
-					if (w->SaveWorld(fname))	{
-						cout << "Сохранено" << endl;
-					}
-					else {
-						cout << "Не сохранено" << endl;
-					}
-					delete[] fname;
-					system("pause");
-				}
-				else if(c == 27)	{
-					break;
-				}
-			}
-			else if (c == 27)	{
-				break;
-			}
-		}
-		system("CLS");		//	чистим экран
-
-		std::cout << "1 - сохранить; 2 - пауза; Esc - выход\n";
-		w->View();			//	выводим
-
-	} while (w->PerformStep());
-	delete w;
-	delete[] fname;
-	system("pause");
-	return false;
+    QApplication app(argc, argv);
+    srand((int)time(NULL));
+    Window w;
+    w.show();
+    return app.exec();
 }
