ddc669794af4d7a9fb3065f80f7a3acb0dcb8ae0
An2An <an2an96@bk.ru>
2015-05-26 19:08:27 +0300

    Update world.cpp

--- world.cpp
+++ world.cpp
@@ -2,7 +2,7 @@
 
 World::World(int x, int y, int PredPerCent, int VictPerCent)
 {
-	sizeX = x, sizeY = y;
+    sizeX = x, sizeY = y;
     GenerateEntitys(PredPerCent, VictPerCent);
 }
 
@@ -14,78 +14,44 @@ void World::GenerateEntitys(int PredPerCent, int VictPerCent)
 
     while (PredCount)
     {
-        if(CreateEntity(ENTITY_PREDATORS, random(0, sizeX), random(0, sizeY)))
+        if(CreateEntity(ENTITY_PREDATORS, random(0, sizeX - 1), random(0, sizeY - 1)))
             PredCount--;
-	}
+    }
     while (VictCount)
     {
-        if(CreateEntity(ENTITY_VICTIMS, random(0, sizeX), random(0, sizeY)))
+        if(CreateEntity(ENTITY_VICTIMS, random(0, sizeX - 1), random(0, sizeY - 1)))
             VictCount--;
     }
 }
 
 bool World::CreateEntity(E_ENTITY type, int x, int y)
 {
-	if (x >= 0 && x < sizeX && y >= 0 && y < sizeY && InCell(x, y) == ENTITY_NONE)
-	{
-		if (type == ENTITY_PREDATORS)	{
-			gEntitys[Cell(x, y)] = std::make_unique<World::Predator>(this, x, y);
-			return true;
-		}
-		else if (type == ENTITY_VICTIMS)	{
-			gEntitys[Cell(x, y)] = std::make_unique<World::Victim>(this, x, y);
-			return true;
-		}
-	}
-	return false;
+    if (x >= 0 && x < sizeX && y >= 0 && y < sizeY && _InCell(gEntitys, x, y) == ENTITY_NONE)
+    {
+        if (type == ENTITY_PREDATORS)	{
+            gEntitys[Cell(x, y)] = std::make_unique<World::Predator>(this, x, y);
+            return true;
+        }
+        else if (type == ENTITY_VICTIMS)	{
+            gEntitys[Cell(x, y)] = std::make_unique<World::Victim>(this, x, y);
+            return true;
+        }
+    }
+    return false;
 }
 
 E_STEP World::PerformStep()
 {
-    //	@Step Victims
-    if (gCycle == ENTITY_VICTIMS)	{
-
-        for (auto i = gEntitys.begin(); i != gEntitys.end(); i = gEntitys.begin())
-        {
-            if (dynamic_cast<Victim*>(i->second.get()))	{
-                int cell = i->second->Step();
-                gInterim[cell] = std::move(gEntitys[i->first]);
-                gEntitys.erase(i);
-                gInterim[cell].get()->Birth();
-                continue;
-            }
-            else if (dynamic_cast<Predator*>(i->second.get()))	{
-                gInterim[i->first] = std::move(gEntitys[i->first]);
-            }
+    for (auto i = gEntitys.begin(); i != gEntitys.end(); i = gEntitys.begin())
+    {
+        if (EntityStep(i))	{
             gEntitys.erase(i);
         }
-        gCycle = ENTITY_PREDATORS;
-    }
-
-//	@Step Predators
-    else if (gCycle == ENTITY_PREDATORS)	{
-
-        for (auto i = gEntitys.begin(); i != gEntitys.end(); i = gEntitys.begin())
-        {
-            if (dynamic_cast<Predator*>(i->second.get()))	{
-                int cell = i->second->Step();
-                if (cell != (-1))	{
-                    gInterim[cell] = std::move(gEntitys[i->first]);
-                    gEntitys.erase(i);
-                    gInterim[cell].get()->Birth();
-                    continue;
-                }
-            }
-            else if (dynamic_cast<Victim*>(i->second.get()))	{
-                gInterim[i->first] = std::move(gEntitys[i->first]);
-            }
-            gEntitys.erase(i);
-        }
-        gCycle = ENTITY_VICTIMS;
     }
     gInterim.swap(gEntitys);
     gInterim.clear();
 
+    gCycle = (gCycle == ENTITY_PREDATORS) ? ENTITY_VICTIMS : ENTITY_PREDATORS;
     if(predatorsCount == 0 && victimsCount == (sizeX * sizeY))
     {
         return STEP_VICTIMS_WIN;
@@ -100,79 +66,70 @@ E_STEP World::PerformStep()
     }
 }
 
+bool World::EntityStep(std::map<int, std::unique_ptr<World::Entity>>::iterator cell)
+{
+    if (cell->second.get())	{
+        if (gCycle == cell->second->Type())
+        {
+            int nCell = cell->second->Step();
+            if (nCell != (-1))	{
+                gInterim[nCell] = std::move(gEntitys[cell->first]);
+                gEntitys.erase(cell);
+                gInterim[nCell].get()->Birth();
+                return false;
+            }
+        }
+        else {
+            gInterim[cell->first] = std::move(gEntitys[cell->first]);
+        }
+    }
+    return true;
+}
+
 bool World::Calculate(int curx, int cury, std::vector<int>& pos)
 {
-	for (int i = cury - 1; i <= cury + 1; i++)	{
-		for (int j = curx - 1; j <= curx + 1; j++)	{
-			if (i < 0 || j < 0 || j >= sizeX || i >= sizeY || (j == curx && i == cury))	{
-				continue;
-			}
-			pos.push_back(Cell(j, i));
-		}
-	}
-	return pos.size() ? true : false;
+    for (int i = cury - 1; i <= cury + 1; i++)	{
+        for (int j = curx - 1; j <= curx + 1; j++)	{
+            if (i < 0 || j < 0 || j >= sizeX || i >= sizeY || (j == curx && i == cury))	{
+                continue;
+            }
+            pos.push_back(Cell(j, i));
+        }
+    }
+    return pos.size() ? true : false;
 }
 
 void World::Size(int size, int& sx, int& sy)
 {
-	sy = size / sizeX; sx = size % sizeX;
+    sy = size / sizeX; sx = size % sizeX;
 }
 
 int World::Cell(int x, int y)
 {
-	return (y * sizeX + x);
+    return (y * sizeX + x);
 }
 
 E_ENTITY World::InCell(int x, int y)
 {
-	if (gEntitys[Cell(x, y)]) {
-		if (dynamic_cast<Victim*>(gEntitys[Cell(x, y)].get()))	{
-			return ENTITY_VICTIMS;
-		}
-		else if (dynamic_cast<Predator*>(gEntitys[Cell(x, y)].get()))	{
-			return ENTITY_PREDATORS;
-		}
-	}
-	return ENTITY_NONE;
+    return _InCell(gEntitys, x, y);
 }
 
-E_ENTITY World::InInterim(int x, int y)
+E_ENTITY World::_InCell(std::map<int, std::unique_ptr<World::Entity>>& object, int x, int y)
 {
-	if (gInterim[Cell(x, y)])	{
-		if (dynamic_cast<Victim*>(gInterim[Cell(x, y)].get()))	{
-			return ENTITY_VICTIMS;
-		}
-		else if (dynamic_cast<Predator*>(gInterim[Cell(x, y)].get()))	{
-			return ENTITY_PREDATORS;
-		}
-	}
-	return ENTITY_NONE;
+    if (object[Cell(x, y)]) {
+        if (dynamic_cast<Victim*>(object[Cell(x, y)].get()))	{
+            return ENTITY_VICTIMS;
+        }
+        else if (dynamic_cast<Predator*>(object[Cell(x, y)].get()))	{
+            return ENTITY_PREDATORS;
+        }
+    }
+    return ENTITY_NONE;
 }
 
-void World::View()
+Coord World::GetWorldSize()
 {
-	for (int x = 0; x < sizeX + 2; x++)	std::cout << "_";
-	std::cout << std::endl;
-	for (int y = 0; y < sizeY; y++)	{
-		std::cout << "|";
-		for (int x = 0; x < sizeX; x++)	{
-			if (gEntitys[Cell(x, y)])	{
-				gEntitys[Cell(x, y)]->View();
-			} else {
-				std::cout << " ";
-			}
-		}
-		std::cout << "|" << std::endl;
-	}
-	for (int x = 0; x < sizeX + 2; x++)	std::cout << "_";
-
-	std::cout << std::endl << "Predators: " << predatorsCount << ", Victims: " << victimsCount << std::endl;
-}
-
-WorldSize World::GetWorldSize()
-{
-    WorldSize ws;
-    ws.SizeX = sizeX;
-    ws.SizeY = sizeY;
+    Coord ws;
+    ws.Set(sizeX, sizeY);
     return ws;
 }
