29fb10ecaa8feb7810854c3070ac4c201583796c
An2An <an2an96@bk.ru>
2015-05-26 19:09:10 +0300

    Update victim.cpp

--- victim.cpp
+++ victim.cpp
@@ -1,24 +1,27 @@
 #include "world.h"
 
 World::Victim::Victim(World* world, int x, int y, int birtch_ac, int birtch_c, int poisoning_c) :
-	Entity(world, x, y, birtch_ac, birtch_c)
+    Entity(world, x, y, birtch_ac, birtch_c)
 {
-	ChancePoisoning = poisoning_c;
-
-	gWorld->victimsCount++;
+    ChancePoisoning = poisoning_c;
+    gWorld->victimsCount++;
 }
 
 World::Victim::Victim(World* world, int x, int y) :
-	Entity(world, x, y)
+    Entity(world, x, y)
 {
-	ChancePoisoning = random(0, 60);
-
-	gWorld->victimsCount++;
+    ChancePoisoning = random(0, 60);
+    gWorld->victimsCount++;
 }
 
 World::Victim::~Victim()
 {
-	gWorld->victimsCount--;
+    gWorld->victimsCount--;
+}
+
+E_ENTITY World::Victim::Type()
+{
+    return ENTITY_VICTIMS;
 }
 
 int World::Victim::Step()
@@ -26,39 +29,18 @@ int World::Victim::Step()
     int bufX, bufY, rand;
     std::vector<int> pos;
     //	подбираем позицию для хода
-    if (gWorld->Calculate(this->pos[0], this->pos[1], pos))	{
+    if (gWorld->Calculate(position.x, position.y, pos))	{
         do
         {
             rand = random(0, (int)pos.size() - 1);
             gWorld->Size(pos[rand], bufX, bufY);
 
             //	Если в клетке никого нет
-            if (gWorld->InCell(bufX, bufY) == ENTITY_NONE && gWorld->InInterim(bufX, bufY) == ENTITY_NONE)	{
+            if (gWorld->_InCell(gWorld->gEntitys, bufX, bufY) == ENTITY_NONE && gWorld->_InCell(gWorld->gInterim, bufX, bufY) == ENTITY_NONE)	{
 
-                this->pos[0] = bufX; this->pos[1] = bufY;
+                position.Set(bufX, bufY);
 
                 ActionForBirth++;
-                /*if (++ActionForBirth >= BirthActionCount && random(0, 100) <= ChanceBirth)	{
-                    //	рождаем новую жертву
-                    ActionForBirth = 0;
-
-                    pos.clear();
-                    if (gWorld->Calculate(this->pos[0], this->pos[1], pos))	{
-                        do
-                        {
-                            rand = random(0, pos.size() - 1);
-                            gWorld->Size(pos[rand], bufX, bufY);
-
-                            if (gWorld->InCell(bufX, bufY) == ENTITY_NONE && gWorld->InInterim(bufX, bufY) == ENTITY_NONE)	{
-                                gWorld->CreateEntity(ENTITY_VICTIMS, bufX, bufY);
-                                break;
-                            }
-                            else {
-                                pos.erase(pos.begin() + rand);
-                            }
-                        } while (pos.size());
-                    }
-                }  */
                 break;
             }
             else	{
@@ -66,7 +48,7 @@ int World::Victim::Step()
             }
         } while (pos.size());
     }
-    return gWorld->Cell(this->pos[0], this->pos[1]);
+    return gWorld->Cell(position.x, position.y);
 }
 
 void World::Victim::Birth()
@@ -74,18 +56,17 @@ void World::Victim::Birth()
     if (ActionForBirth >= BirthActionCount && random(0, 100) <= ChanceBirth)	{
         int bufX, bufY, rand;
         std::vector<int> pos;
-
         //	рождаем новую жертву
         ActionForBirth = 0;
 
         pos.clear();
-        if (gWorld->Calculate(this->pos[0], this->pos[1], pos))	{
+        if (gWorld->Calculate(position.x, position.y, pos))	{
             do
             {
                 rand = random(0, (int)pos.size() - 1);
                 gWorld->Size(pos[rand], bufX, bufY);
 
-                if (gWorld->InCell(bufX, bufY) == ENTITY_NONE && gWorld->InInterim(bufX, bufY) == ENTITY_NONE)	{
+                if (gWorld->_InCell(gWorld->gEntitys, bufX, bufY) == ENTITY_NONE && gWorld->_InCell(gWorld->gInterim, bufX, bufY) == ENTITY_NONE)	{
                     gWorld->CreateEntity(ENTITY_VICTIMS, bufX, bufY);
                     break;
                 }
@@ -98,20 +79,15 @@ void World::Victim::Birth()
 }
 
 int World::Victim::GetPoisoning()	{
-	return ChancePoisoning;
-}
-
-void World::Victim::View()
-{
-	printf("O");
+    return ChancePoisoning;
 }
 
 void World::Victim::PrintData(std::ofstream& out)
 {
-	out << ActionForBirth << " " << BirthActionCount << " " << ChanceBirth << " " << ChancePoisoning;
-};
+    out << ActionForBirth << " " << BirthActionCount << " " << ChanceBirth << " " << ChancePoisoning;
+}
 
 void World::Victim::ReadData(std::ifstream& in)
 {
-	in >> ActionForBirth >> BirthActionCount >> ChanceBirth >> ChancePoisoning;
-};
+    in >> ActionForBirth >> BirthActionCount >> ChanceBirth >> ChancePoisoning;
+}
